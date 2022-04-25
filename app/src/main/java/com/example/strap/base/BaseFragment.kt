479b81dd41cfebe04b0f51c0d1dcee981ea6b252
k906506 <k906506@naver.com>
2022-04-25 23:17:16 +0900

    [feat] BaseActivity, BaseFragment 구현

---
+++ app/src/main/java/com/example/strap/base/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.example.strap.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    abstract val FRAGMENT_TAG: String
+
+    private var _binding: T? = null
+    val binding get() = _binding ?: error("Not Initialized")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = this@BaseFragment
+        return binding.root
+    }
+}
