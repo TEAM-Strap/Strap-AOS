3047f23f5afc485df77118c668c4cd5bace32f68
k906506 <k906506@naver.com>
2022-05-18 12:29:05 +0900

    [feat] 로그인, 로그아웃, 회원가입 기능 구현

--- app/src/main/java/com/example/strap/viewmodel/activity/MainActivity.kt
+++ app/src/main/java/com/example/strap/viewmodel/activity/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.strap.viewmodel.activity
 
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.strap.R
 import com.example.strap.base.BaseActivity
@@ -10,10 +11,20 @@ import com.example.strap.viewmodel.fragment.CalendarFragment
 import com.example.strap.viewmodel.fragment.GroupFragment
 import com.example.strap.viewmodel.fragment.HomeFragment
 import com.example.strap.viewmodel.fragment.MypageFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override val ACTIVITY_TAG: String
         get() = R.layout.activity_main.toString()
+
+    private val _auth by lazy {
+        Firebase.auth
+    }
+
+    val auth: FirebaseAuth
+        get() = _auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,10 +58,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         }
     }
 
-    private fun changeFragment(fragment: Fragment) {
+    fun changeFragment(fragment: Fragment) {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .commit()
+    }
+
+    fun saveAndChangeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun showToast(context: Context, text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 }
