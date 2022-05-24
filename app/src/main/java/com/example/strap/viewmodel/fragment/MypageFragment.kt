ebaf8816b5118b3c64f73083cd757cd781258cff
k906506 <k906506@naver.com>
2022-05-24 11:28:20 +0900

    [feat] 마이페이지 로그아웃 임시 구현

--- app/src/main/java/com/example/strap/viewmodel/fragment/MypageFragment.kt
+++ app/src/main/java/com/example/strap/viewmodel/fragment/MypageFragment.kt
@@ -1,15 +1,29 @@
 package com.example.strap.viewmodel.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.example.strap.R
 import com.example.strap.base.BaseFragment
 import com.example.strap.databinding.FragmentMypageBinding
+import com.example.strap.viewmodel.activity.LoginActivity
 
 class MypageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
     override val FRAGMENT_TAG: String
         get() = "MypageFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        bindViews()
+    }
+
+    private fun bindViews() {
+        binding.btnLogout.setOnClickListener {
+            activity.auth.signOut()
+            activity.showToast(requireContext(), "로그아웃에 성공하였습니다.")
+
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+            activity.finish()
+        }
     }
 }
